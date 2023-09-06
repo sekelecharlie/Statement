37d5c1e6d0143f50b178d5b4dd4ff2ca5e705c84
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-06 20:51:36 +0200

    Add project files.

---
+++ Statement/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnGreeting_Click(sender As Object, e As EventArgs) Handles btnGreeting.Click
+        Dim country As String
+
+        country = txtCountry.Text.ToUpper
+        If country = "SOUTH AFRICA" Then
+            MsgBox("THOBELA")
+
+        ElseIf country = "SPAIN" Then
+            MsgBox("OLE")
+
+        ElseIf country = "ZIMBABWE" Then
+            MsgBox("ZWIRIBO")
+
+        Else
+            MsgBox("Hello")
+
+        End If
+
+    End Sub
+End Class
